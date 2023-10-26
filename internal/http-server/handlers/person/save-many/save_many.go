cc388f2221a88cbfd3cc2ad028bc8c4af9d44e99
ToshaRotten <anton.kozlov2003@mail.ru>
2023-10-27 00:44:22 +0700

    fix op, add parallel requests to apis

--- internal/http-server/handlers/person/save-many/save_many.go
+++ internal/http-server/handlers/person/save-many/save_many.go
@@ -4,9 +4,13 @@ import (
 	"github.com/go-playground/validator/v10"
 	"log/slog"
 	"net/http"
+	"people-api/internal/http-client/handlers/get_age"
+	"people-api/internal/http-client/handlers/get_gender"
+	"people-api/internal/http-client/handlers/get_nationality"
 	"people-api/internal/http-server/utils/render"
 	"people-api/internal/models/person"
 	"people-api/internal/models/response"
+	"sync"
 )
 
 type Request struct {
@@ -18,12 +22,12 @@ type Response struct {
 }
 
 type PersonSaver interface {
-	SavePersons(persons []person.Person) error
+	SavePerson(person person.Person) error
 }
 
 func SaveMany(log *slog.Logger, personSaver PersonSaver) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		const op = "handlers.url.save.New"
+		const op = "handlers.url.SaveMany"
 		log = log.With(slog.String("op", op))
 
 		var req Request
@@ -43,13 +47,49 @@ func SaveMany(log *slog.Logger, personSaver PersonSaver) http.HandlerFunc {
 			return
 		}
 
-		err = personSaver.SavePersons(req.Items)
-		if err != nil {
-			log.Error("err:", err)
-			render.JSON(w, response.Error())
-			return
-		}
+		for _, item := range req.Items {
+			wg := sync.WaitGroup{}
+			wg.Add(3)
 
+			personToSave := person.Person{
+				Name:       item.Name,
+				Surname:    item.Surname,
+				Patronymic: item.Patronymic,
+			}
+
+			go func() {
+				personToSave.Age, err = get_age.GetAge(item.Name)
+				if err != nil {
+					return
+				}
+				wg.Done()
+			}()
+
+			go func() {
+				personToSave.Sex, err = get_gender.GetGender(item.Name)
+				if err != nil {
+					return
+				}
+				wg.Done()
+			}()
+
+			go func() {
+				nationalities, err := get_nationality.GetNationality(item.Name)
+				personToSave.Nationality = nationalities[0]
+				if err != nil {
+					return
+				}
+				wg.Done()
+			}()
+			wg.Wait()
+
+			err = personSaver.SavePerson(personToSave)
+			if err != nil {
+				log.Error("err:", err)
+				render.JSON(w, response.Error())
+				return
+			}
+		}
 		log.Info("persons added")
 
 		render.JSON(w, response.OK())
